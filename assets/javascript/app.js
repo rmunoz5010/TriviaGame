4ea7dfd6bba5a46c1cb63e4033cbd51af91f592e
Ramon Munoz <rgm_munoz@mac.com>
2017-12-14 02:03:32 -0500

    v1.4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,38 @@ function startGame(){
         $("#question").html(questions[0]) 
         var answers= ["A Shrubbery", "Money", "Pigs", "Nothing at All!"];
         for(var i = 0; i < answers.length; i++){
-        $("#answers").append(htmlAnswerArray[i]);
-        $("#answer"+[i]).html(answers[i]);
+            $("#answers").append(htmlAnswerArray[i]);
+            $("#answer"+[i]).html(answers[i]);
         }
-        // if()
+
+        $("#answer0").on("click", function(){
+            $(".jumbotron").css("background-color", "green");
+            $("#answers").html("<p> The Knights who say Ni requests a shrubbery to allow someone to pass through. </p>")
+            $("#question").html("CORRECT!")
+
+        });
+
+        $("#answer1").on("click", function(){
+            $(".jumbotron").css("background-color", "red");
+            $("#answers").html("<p> The Knights who say Ni requests a shrubbery to allow someone to pass through. </p>")
+            $("#question").html("INCORRECT!")
+
+        });
+
+        $("#answer2").on("click", function(){
+            $(".jumbotron").css("background-color", "red");
+            $("#answers").html("<p> The Knights who say Ni requests a shrubbery to allow someone to pass through. </p>")
+            $("#question").html("INCORRECT!")
+
+        });
+
+        $("#answer3").on("click", function(){
+            $(".jumbotron").css("background-color", "red");
+            $("#answers").html("<p> The Knights who say Ni requests a shrubbery to allow someone to pass through. </p>")
+            $("#question").html("INCORRECT!")
+
+        });
+        
     }
 
 
